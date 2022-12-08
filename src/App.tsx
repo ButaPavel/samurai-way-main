9c464d199cc5770e6cb5e129a30a3e873444c6db
ButaPavel <buta.pavel84@gmail.com>
2022-12-08 19:32:00 +0300

    lesson 7

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,31 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (<div>
-          <div>one div</div>
-    <div className="App">
-    <ul>
-        <li>css</li>
-        <li>html</li>
-        <li>js</li>
-        <li>react</li>
-    </ul>
-    </div>
+const App = () => {
+  return (
+      <div>
+          <Header />
+            <Tehnologies />
       </div>
   );
 }
-
+const Header = () => {
+  return (
+      <div>
+          <a href={'#s'}>Home</a>
+          <a href={'#s'}>News Feed</a>
+          <a href={'#s'}>Messages</a>
+      </div>
+  )
+}
+const Tehnologies = () => {
+    return (<div>
+        <ul>
+            <li>css</li>
+            <li>html</li>
+            <li>js</li>
+            <li>react</li>
+        </ul>
+    </div>)
+}
 export default App;
